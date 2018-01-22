3e83bc95cdb22205e2216cad3e23936a52042023
Kwok <astleykwok@gmail.com>
2018-01-22 23:17:25 +0800

    change angular app name

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ var config = require('./config/database');
 var mongoose = require('mongoose');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
@@ -37,10 +36,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, '/client/dist/')));
+app.use(express.static(path.join(__dirname, '/angular-app/dist/')));
 
 app.use('/', index);
-//app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
