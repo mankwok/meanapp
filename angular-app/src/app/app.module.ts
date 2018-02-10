c205edc696ff0b30ae72aa7c43727653c7d3044e
Kwok <astleykwok@gmail.com>
2018-02-10 22:28:54 +0800

    Remove navbar

--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -4,26 +4,26 @@ import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ErrorNotFoundComponent } from './components/error-not-found/error-not-found.component';
-
+import { AuthService } from './services/auth.service';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
     HomeComponent,
     DashboardComponent,
-    ErrorNotFoundComponent
+    ErrorNotFoundComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
