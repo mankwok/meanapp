212652e33c4a4ff94b80a73a2cb337d60b08d3b6
Kwok <astleykwok@gmail.com>
2018-02-28 23:28:14 +0800

    Add progress bar

--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Router, Routes, NavigationStart, NavigationEnd } from '@angular/router';
+import { NgProgress, NgProgressModule} from 'ngx-progressbar';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
@@ -9,6 +11,8 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ErrorNotFoundComponent } from './components/error-not-found/error-not-found.component';
 import { AuthService } from './services/auth.service';
 import { LoginComponent } from './components/login/login.component';
+
+import 'rxjs/add/operator/filter';
 
 @NgModule({
   declarations: [
@@ -21,9 +25,23 @@ import { LoginComponent } from './components/login/login.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    NgProgressModule
   ],
-  providers: [AuthService],
+  providers: [
+    AuthService,  
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private router: Router, private ngProgress: NgProgress) { 
+
+    router.events
+       .filter(event => event instanceof NavigationStart)
+       .subscribe(() => ngProgress.start());
+
+    router.events
+       .filter(event => event instanceof NavigationEnd)
+       .subscribe(() => ngProgress.done());
+  }
+}
