6550f280ecc572f2338d4f3c5d9bc4703472b6e8
Kwok <astleykwok@gmail.com>
2018-04-10 22:36:58 +0800

    clean up

--- routes/authentication.js
+++ routes/authentication.js
@@ -1,95 +1,81 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const path = require("path");
-const jwt = require("jsonwebtoken");
+const jwt = require('jsonwebtoken');
 
-const User = require("../models/user");
-const config = require("../config/database");
+const User = require('../models/user');
+const config = require('../config/database');
 
-const userAuthMw = require("../middleware/user-auth-middleware");
-
-router.post("/register", (req, res) => {
-  // Check if email was provided
+router.post('/register', (req, res) => {
   if (!req.body.email) {
     res.json({
       success: false,
-      message: "You must provide an e-mail"
-    }); // Return error
+      message: 'You must provide an e-mail'
+    });
   } else {
-    // Check if username was provided
     if (!req.body.username) {
       res.json({
         success: false,
-        message: "You must provide a username"
-      }); // Return error
+        message: 'You must provide a username'
+      });
     } else {
-      // Check if password was provided
       if (!req.body.password) {
         res.json({
           success: false,
-          message: "You must provide a password"
-        }); // Return error
+          message: 'You must provide a password'
+        });
       } else {
-        // Create new user object and apply user input
         let user = new User({
           email: req.body.email.toLowerCase(),
           username: req.body.username.toLowerCase(),
           password: req.body.password
         });
-        // Save user to database
         user.save(err => {
-          // Check if error occured
           if (err) {
-            // Check if error is an error indicating duplicate account
             if (err.code === 11000) {
               res.json({
                 success: false,
-                message: "Username or e-mail already exists"
-              }); // Return error
+                message: 'Username or e-mail already exists'
+              });
             } else {
-              // Check if error is a validation rror
               if (err.errors) {
-                // Check if validation error is in the email field
                 if (err.errors.email) {
                   res.json({
                     success: false,
                     message: err.errors.email.message
-                  }); // Return error
+                  });
                 } else {
-                  // Check if validation error is in the username field
                   if (err.errors.username) {
                     res.json({
                       success: false,
                       message: err.errors.username.message
-                    }); // Return error
+                    });
                   } else {
-                    // Check if validation error is in the password field
                     if (err.errors.password) {
                       res.json({
                         success: false,
                         message: err.errors.password.message
-                      }); // Return error
+                      });
                     } else {
                       res.json({
                         success: false,
                         message: err
-                      }); // Return any other error not already covered
+                      });
                     }
                   }
                 }
               } else {
                 res.json({
                   success: false,
-                  message: "Could not save user. Error: ",
+                  message: 'Could not save user. Error: ',
                   err
-                }); // Return error if not related to validation
+                });
               }
             }
           } else {
             res.json({
               success: true,
-              message: "Acount registered!"
-            }); // Return success
+              message: 'Acount registered!'
+            });
           }
         });
       }
@@ -97,16 +83,16 @@ router.post("/register", (req, res) => {
   }
 });
 
-router.post("/login", (req, res) => {
+router.post('/login', (req, res) => {
   if (!req.body.username) {
     res.json({
       success: false,
-      message: "No username given!"
+      message: 'No username given!'
     });
   } else if (!req.body.password) {
     res.json({
       success: false,
-      message: "No password given!"
+      message: 'No password given!'
     });
   } else {
     User.findOne(
@@ -122,7 +108,7 @@ router.post("/login", (req, res) => {
         } else if (!user) {
           res.json({
             success: false,
-            message: "Invalid username or password!"
+            message: 'Invalid username or password!'
           });
         } else {
           const validPassword = user.comparePassword(req.body.password);
@@ -133,12 +119,12 @@ router.post("/login", (req, res) => {
               },
               config.secret,
               {
-                expiresIn: "24h"
+                expiresIn: '24h'
               }
             );
             res.json({
               success: true,
-              message: "Login success!",
+              message: 'Login success!',
               token: token,
               user: {
                 username: user.username
@@ -147,7 +133,7 @@ router.post("/login", (req, res) => {
           } else {
             res.json({
               success: false,
-              message: "Invalid username or password!"
+              message: 'Invalid username or password!'
             });
           }
         }
