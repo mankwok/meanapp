9a3804423e0348fa8cf6a819475fb4195fc4183f
Kwok <astleykwok@gmail.com>
2018-04-15 18:31:00 +0800

    Fix comparison

--- routes/posts.js
+++ routes/posts.js
@@ -85,20 +85,20 @@ router.get('/single/:id', (req, res) => {
     res.json({ success: false, message: 'Invalid post id' });
   } else {
     Post.findOne({ _id: req.params.id })
-    .populate('createdBy', 'engName')
-    .populate('comments.commentBy', 'engName')
-    .populate('likedBy', 'engName')
-    .exec((err, post) => {
-      if (err) {
-        res.json({ success: false, message: err });
-      } else {
-        if (!post) {
-          res.json({ success: false, message: 'No post found' });
+      .populate('createdBy', 'engName')
+      .populate('comments.commentBy', 'engName')
+      .populate('likedBy', 'engName')
+      .exec((err, post) => {
+        if (err) {
+          res.json({ success: false, message: err });
         } else {
-          res.json({ success: true, post: post });
+          if (!post) {
+            res.json({ success: false, message: 'No post found' });
+          } else {
+            res.json({ success: true, post: post });
+          }
         }
-      }
-    })
+      });
   }
 });
 
@@ -151,15 +151,20 @@ router.put('/likePost', (req, res) => {
       if (err) {
         res.json({ success: false, message: 'Invalid post id' });
       } else {
-        User.findOne({ _id: req.decoded.userId }, (err, user) => {
-          if (err) {
-            res.json({ success: false, message: 'Invalid user id' });
-          } else {
-            if (!user) {
-              res.json({ success: false, message: 'User not found' });
+        if (!post) {
+          res.json({ success: false, message: 'Invalid post id' });
+        } else {
+          User.findOne({ _id: req.decoded.userId }, (err, user) => {
+            if (err) {
+              res.json({ success: false, message: 'Invalid user id' });
             } else {
-                console.log("ttyyy");
-                if (post.likedBy.includes(user._id)) {
+              if (!user) {
+                res.json({ success: false, message: 'User not found' });
+              } else {
+                var isInArray = post.likedBy.some(function(likeBy) {
+                  return likeBy.equals(user._id);
+                });
+                if (isInArray) {
                   const index = post.likedBy.indexOf(user._id);
                   post.likedBy.splice(index, 1);
                   post.save(err => {
@@ -187,8 +192,8 @@ router.put('/likePost', (req, res) => {
                 }
               }
             }
-          
-        });
+          });
+        }
       }
     });
   }
